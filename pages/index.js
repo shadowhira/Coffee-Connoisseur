488a022b4c884a92efc18b62d6de7b3bd38146cf
shadowhira <thanhoc890@gmail.com>
2024-11-14 21:49:06 +0700

    add coffee store page

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,6 @@ export async function getStaticProps(context) {
 }
 
 export default function Home(props) {
-  console.log("props: ", props);
   const handleOnBannerBtnClick = () => {
     console.log("Banner here");
   };
