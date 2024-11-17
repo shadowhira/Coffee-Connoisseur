41e11bece614fcd59d5c5919cd99d1e522202770
shadowhira <thanhoc890@gmail.com>
2024-11-17 20:28:12 +0700

    add data from api and use at coffee stores page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -7,20 +7,23 @@ import cls from "classnames";
 
 import coffeeStoresData from "../../data/coffee-stores.json";
 import styles from "../../styles/coffee-store.module.css";
+import { fetchCoffeeStores } from "@/lib/coffee-stores";
 
-export function getStaticProps(staticProps) {
+export async function getStaticProps(staticProps) {
   const params = staticProps.params;
+  const coffeeStores = await fetchCoffeeStores();
   return {
     props: {
-      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+      coffeeStore: coffeeStores.find((coffeeStore) => {
         return coffeeStore.id.toString() === params.id;
       }),
     },
   };
 }
 
-export function getStaticPaths() {
-  const paths = coffeeStoresData.map((coffeeStore) => {
+export async function getStaticPaths() {
+  const coffeeStores = await fetchCoffeeStores();
+  const paths = coffeeStores.map((coffeeStore) => {
     return {
       params: {
         id: coffeeStore.id.toString(),
@@ -40,7 +43,7 @@ const CoffeeStore = (props) => {
     return <div>Loading...</div>;
   }
 
-  const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
+  const { address, locality, name, imgUrl } = props.coffeeStore;
 
   const handleUpvoteButton = () => {
     console.log("Up vote!");
@@ -54,14 +57,17 @@ const CoffeeStore = (props) => {
       <div className={styles.container}>
         <div className={styles.col1}>
           <div className={styles.backToHomeLink}>
-            <Link href="/">Back to home</Link>
+            <Link href="/">◄ Back to home</Link>
           </div>
           <div className={styles.nameWrapper}>
             <h1 className={styles.name}>{name}</h1>
           </div>
           <div className={styles.storeImgWrapper}>
             <Image
-              src={imgUrl}
+              src={
+                imgUrl ||
+                "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+              }
               width={600}
               height={360}
               className={styles.storeImg}
@@ -76,7 +82,7 @@ const CoffeeStore = (props) => {
           </div>
           <div className={styles.iconWrapper}>
             <Image src="/static/icons/nearMe.svg" width="24" height="24" />
-            <p className={styles.text}>{neighbourhood}</p>
+            <p className={styles.text}>{locality}</p>
           </div>
           <div className={styles.iconWrapper}>
             <Image src="/static/icons/star.svg" width="24" height="24" />
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,14 @@ import styles from "@/styles/Home.module.css";
 import Head from "next/head";
 import Image from "next/image";
 import coffeeStoresData from "../data/coffee-stores.json";
+import { fetchCoffeeStores } from "@/lib/coffee-stores";
 
 export async function getStaticProps(context) {
+  const coffeeStores = await fetchCoffeeStores();
+
   return {
     props: {
-      coffeeStores: coffeeStoresData,
+      coffeeStores,
     },
   };
 }
@@ -42,7 +45,10 @@ export default function Home(props) {
                     <Card
                       key={coffeeStore.id}
                       name={coffeeStore.name}
-                      imgUrl={coffeeStore.imgUrl}
+                      imgUrl={
+                        coffeeStore.imgUrl ||
+                        "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                      }
                       href={`/coffee-store/${coffeeStore.id}`}
                       className={styles.card}
                     />
